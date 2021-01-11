cf10b0170a1573e2b448fdab3074768819fea8e7
cscripnic <cscripnic@DCCNOTE235>
2021-01-11 14:03:39 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraEstags/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace CalculadoraEstags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnMultiplicacao = new System.Windows.Forms.Button();
+            this.btnMaiorValor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor 2:";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(83, 36);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(146, 26);
+            this.txtValor1.TabIndex = 2;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(83, 110);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(146, 26);
+            this.txtValor2.TabIndex = 3;
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(17, 217);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(275, 41);
+            this.btnSoma.TabIndex = 4;
+            this.btnSoma.Text = "Soma";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnMultiplicacao
+            // 
+            this.btnMultiplicacao.Location = new System.Drawing.Point(17, 277);
+            this.btnMultiplicacao.Name = "btnMultiplicacao";
+            this.btnMultiplicacao.Size = new System.Drawing.Size(275, 41);
+            this.btnMultiplicacao.TabIndex = 4;
+            this.btnMultiplicacao.Text = "Multiplicação";
+            this.btnMultiplicacao.UseVisualStyleBackColor = true;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.btnMultiplicacao_Click);
+            // 
+            // btnMaiorValor
+            // 
+            this.btnMaiorValor.Location = new System.Drawing.Point(17, 341);
+            this.btnMaiorValor.Name = "btnMaiorValor";
+            this.btnMaiorValor.Size = new System.Drawing.Size(275, 41);
+            this.btnMaiorValor.TabIndex = 4;
+            this.btnMaiorValor.Text = "Maior valor";
+            this.btnMaiorValor.UseVisualStyleBackColor = true;
+            this.btnMaiorValor.Click += new System.EventHandler(this.btnMaiorValor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 450);
+            this.Controls.Add(this.btnMaiorValor);
+            this.Controls.Add(this.btnMultiplicacao);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnMultiplicacao;
+        private System.Windows.Forms.Button btnMaiorValor;
+    }
+}
+
---
+++ CalculadoraEstags/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraEstags
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            int valor1 = Convert.ToInt32(txtValor1.Text);
+
+            int valor2 = Convert.ToInt32(txtValor2.Text);
+
+            int resultado = valor1 + valor2;
+
+            MessageBox.Show(Convert.ToString(resultado));
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            int valor1 = Convert.ToInt32(txtValor1.Text);
+
+            int valor2 = Convert.ToInt32(txtValor2.Text);
+
+            int resultado = valor1 * valor2;
+
+            MessageBox.Show(Convert.ToString(resultado));
+        }
+
+        private void btnMaiorValor_Click(object sender, EventArgs e)
+        {
+            int valor1 = Convert.ToInt32(txtValor1.Text);
+
+            int valor2 = Convert.ToInt32(txtValor2.Text);
+
+            if (valor1 > valor2)
+            {
+                MessageBox.Show("O valor 1 é maior");
+            }
+            else if (valor2 > valor1)
+            {
+                MessageBox.Show("O valor 2 é maior");
+            }
+            else
+            {
+                MessageBox.Show("Os valores são iguais");
+            }
+        }
+    }
+}
